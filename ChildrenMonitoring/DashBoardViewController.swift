8be20ab66aeca483bab01dd3cdf401bf88e1d656
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-11-24 22:33:39 -0600

    Dashboard View controller

--- ChildrenMonitoring/DashBoardViewController.swift
+++ ChildrenMonitoring/DashBoardViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class DashBoardViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var messageOL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        messageOL.text! = "Welcome to ChildMonitor, this is your dashboard!"
     }
     
 
