c69ba6695da4435bbd52cea089d5b80b765b6a88
piyush-1414 <56688656+piyush-1414@users.noreply.github.com>
2025-03-23 22:37:44 -0500

    Update MapsViewController.swift

--- ChildrenMonitoring/MapsViewController.swift
+++ ChildrenMonitoring/MapsViewController.swift
@@ -8,15 +8,23 @@
 import UIKit
 import GoogleMaps
 import CoreLocation
+import FirebaseAuth
+import FirebaseFirestore
+import GooglePlaces
 
 class MapsViewController: UIViewController {
 
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addressLBL: UILabel!
+    
+    var autocompleteController = GMSAutocompleteViewController()
     
     var locationManager = CLLocationManager()
     var location = CLLocation()
     var selectedLocation: CLLocation?
     
+    let model = LocationDetails()
+    var allChildsList: [ChildModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +33,83 @@ class MapsViewController: UIViewController {
         self.initializeLocationManager()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.getMyChilds()
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         locationManager.stopUpdatingLocation()
+        
+    }
+    
+    func getMyChilds() -> Void {
+        
+        let database = Firestore.firestore()
+        let id = Auth.auth().currentUser?.uid ?? ""
+        
+        let docRef = database.collection("Childs")
+            .whereField("parent_id", isEqualTo: id)
+        docRef.addSnapshotListener { (querySnapshot, err) in
+            if let err = err {
+                
+                print("Error getting documents: \(err)")
+                
+            } else {
+                
+                self.allChildsList.removeAll()
+                
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    
+                    let data = document.data()
+                    
+                    var child = ChildModel()
+                    child.id = data["id"] as? String ?? ""
+                    child.parent_id = data["parent_id"] as? String ?? ""
+                    child.parent_name = data["parent_name"] as? String ?? ""
+                    child.name = data["name"] as? String ?? ""
+                    child.age = data["age"] as? String ?? ""
+                    child.email = data["email"] as? String ?? ""
+                    child.image = data["image"] as? String ?? ""
+                    child.lat = data["lat"] as? Double ?? 0.0
+                    child.lng = data["lng"] as? Double ?? 0.0
+                    child.address = data["address"] as? String ?? ""
+                    
+                    self.allChildsList.append(child)
+                }
+                
+                self.showOnMap()
+            }
+        }
+    }
+    
+    func showOnMap() -> Void {
+        
+        self.mapView.clear()
+        var bounds = GMSCoordinateBounds()
+        
+        for child in self.allChildsList {
+            
+            let position = CLLocationCoordinate2D(latitude: child.lat ?? 0.0,
+                                                  longitude: child.lng ?? 0.0)
+            
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: position.latitude,
+                                                     longitude: position.longitude)
+            marker.title = String(format: "%@ \n%@", child.name ?? "", child.address ?? "")
+            marker.map = mapView
+            
+            // Extend bounds to include this marker's position
+            bounds = bounds.includingCoordinate(position)
+        }
+        
+        // Update camera to fit all markers
+        let update = GMSCameraUpdate.fit(bounds, withPadding: 50)
+        mapView.animate(with: update)
     }
     
     
@@ -68,6 +149,17 @@ class MapsViewController: UIViewController {
     @IBAction func current(_ sender: Any) {
         
         self.initializeLocationManager()
+    }
+    
+    
+    @IBAction func address(_ sender: Any) {
+        
+        //self.searchAddress()
+        
+        let autocompleteVC = GMSAutocompleteViewController()
+        autocompleteVC.delegate = self
+        present(autocompleteVC, animated: true, completion: nil)
+        
     }
 }
 
@@ -185,9 +277,126 @@ extension MapsViewController: GMSMapViewDelegate {
                     }
                     print(str ?? "address")
                     
-                    //self.addressTF.text = str
+                    self.addressLBL.text = str
                 }
             }
         })
     }
+}
+
+//
+//extension MapsViewController {
+//    
+//    @objc func addressSelected(place: GMSPlace) -> Void {
+//        
+//        let request = model.getLocationDetail(place: "\(place.placeID ?? "")")
+//        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: .main) { [self] (response, data, error) in
+//            if data == nil {
+//                print("ignore")
+//                
+//            }else {
+//                let dic = model.jsonValue(data)
+//                let result = dic?.value(forKey: "result") as? NSDictionary ?? NSDictionary()
+//                let geometry = result.value(forKey: "geometry") as? NSDictionary ?? NSDictionary()
+//                let location = geometry.value(forKey: "location") as? NSDictionary ?? NSDictionary()
+//                
+//                let lat_Str = "\(String(describing: location["lat"] ?? "0.0"))"
+//                let lng_Str = "\(String(describing: location["lng"] ?? "0.0"))"
+//                
+//                let lat = Double(lat_Str)
+//                let lng = Double(lng_Str)
+//                
+//                let camera = GMSCameraPosition.camera(withLatitude: lat ?? 0.0,
+//                                                      longitude: lng ?? 0.0,
+//                                                      zoom: 15.0)
+//                self.mapView?.animate(to: camera)
+//                
+//                //delegate?.didSelectAddress(lat: lat ?? 0.00, lng: lng ?? 0.00)
+//                //self.navigationController?.popViewController(animated: true)
+//            }
+//        }
+//    }
+//    
+//    func searchAddress() -> Void {
+//        
+//        autocompleteController.delegate = self
+//        let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt64(UInt(GMSPlaceField.name.rawValue) |
+//                                                                   UInt(GMSPlaceField.placeID.rawValue)))
+//        autocompleteController.placeFields = fields
+//        
+//        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+//
+//        // Specify a filter.
+//        let filter = GMSAutocompleteFilter()
+//        //filter.type = .city
+//        filter.country = "USA"
+//        autocompleteController.autocompleteFilter = filter
+//        
+//        // Display the autocomplete view controller.
+//        present(autocompleteController, animated: true, completion: nil)
+//    }
+//}
+//
+//extension MapsViewController: GMSAutocompleteViewControllerDelegate {
+//    
+//    // Handle the user's selection.
+//    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+//        
+//        perform(#selector(addressSelected(place:)), with: place, afterDelay: 0.2)
+//        dismiss(animated: true, completion: nil)
+//    }
+//    
+//    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+//        // TODO: handle the error.
+//        print("Error: ", error.localizedDescription)
+//    }
+//    
+//    // User canceled the operation.
+//    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+//        dismiss(animated: true, completion: nil)
+//    }
+//    
+//    // Turn the network activity indicator on and off again.
+//    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+//    }
+//    
+//    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+//    }
+//}
+
+extension MapsViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // MARK: - GMSAutocompleteViewControllerDelegate Methods
+        func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+            print("Selected Place: \(place.name ?? ""), Address: \(place.formattedAddress ?? ""), Place ID: \(place.placeID ?? "No ID")")
+
+            // Manually fetch place details
+            fetchPlaceDetails(placeID: place.placeID ?? "")
+
+            dismiss(animated: true, completion: nil)
+        }
+        
+        func fetchPlaceDetails(placeID: String) {
+            let placesClient = GMSPlacesClient.shared()
+            
+            placesClient.fetchPlace(fromPlaceID: placeID, placeFields: [.name, .formattedAddress, .coordinate], sessionToken: nil) { (place, error) in
+                if let error = error {
+                    print("Error fetching place details: \(error.localizedDescription)")
+                    return
+                }
+                if let place = place {
+                    print("Fetched Place Details: \(place.name ?? ""), \(place.formattedAddress ?? "")")
+                }
+            }
+        }
+
+        func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+            print("Google Places Error: \(error.localizedDescription)")
+        }
+
+        func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+            dismiss(animated: true, completion: nil)
+        }
 }
