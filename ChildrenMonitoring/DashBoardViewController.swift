af7c2e530eead8b0328f2a3b0ba7fa6a0821d6d1
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2025-03-30 20:06:16 -0500

    Update DashBoardViewController.swift

--- ChildrenMonitoring/DashBoardViewController.swift
+++ ChildrenMonitoring/DashBoardViewController.swift
@@ -14,6 +14,33 @@ class DashBoardViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+
+    // Setup UI Elements
+    func setupUI() {
+        welcomeLabel.text = "Welcome to the Dashboard"
+        childActivityButton.setTitle("View Child Activity", for: .normal)
+        logoutButton.setTitle("Logout", for: .normal)
+        
+        childActivityButton.addTarget(self, action: #selector(viewChildActivity), for: .touchUpInside)
+        logoutButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
+    }
+    
+    // Navigate to Child Activity Screen
+    @objc func viewChildActivity() {
+        performSegue(withIdentifier: "showChildActivity", sender: self)
+    }
+    
+    // Logout and Navigate to Login Screen
+    @objc func logout() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showChildActivity" {
+            // Pass any necessary data to the child activity screen
+        }
+    }
     
 
     /*
